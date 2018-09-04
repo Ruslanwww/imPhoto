1cf261709bb6640828b04ba243b0060e2d07a336
Ruslanwww <oruslano51@gmail.com>
2018-09-04 14:40:54 +0300

    Added subscription functionality

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,7 +1,37 @@
 class ProfilesController < ApplicationController
-before_action :set_user, only: :show
+before_action :set_user
 
   def show 
+  end
+
+  def subscribe
+    if current_user.id == @user.id
+      redirect_to profile_path(@user), notice: 'Вы не можете подписаться сами на себя'
+    else
+      if current_user.subscriptions.exists?(friend_id: @user.id)
+        redirect_to profile_path(@user), notice: 'Вы уже подписаны на данного пользователя'
+      else
+        @subscription = current_user.subscriptions.build
+        @subscription.friend_id = @user.id
+        if @subscription.save
+          redirect_to profile_path(@user), notice: 'Вы подписаны на данного пользователя'
+        end
+      end
+    end
+  end
+
+  def unsubscribe
+    if current_user.id == @user.id
+      redirect_to profile_path(@user), notice: 'Вы не можете отписаться сами от себя'
+    else
+      if current_user.subscriptions.exists?(friend_id: @user.id)
+        @subscription = current_user.subscriptions.find_by_friend_id(@user.id)
+        @subscription.destroy
+        redirect_to profile_path(@user), notice: 'Вы больше не подписаны на данного пользователя'
+      else
+        redirect_to profile_path(@user), notice: 'Вы не были подписаны на данного пользователя'
+      end
+    end
   end
 
 private
